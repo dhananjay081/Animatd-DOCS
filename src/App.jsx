057bd08873a09d0afa1268702cb565b543d98263
dhananjay <dhananjaykushwaha238@gamil.com>
2024-08-03 11:35:20 +0530

    Set all the envirnoment and making the background component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Background from'./components/background'
+function App() {
+  return (
+    <div className='relative w-full h-screen bg-zinc-800'>
+        <Background/>
+        <div className='w-full h-full bg-sky-800'></div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/background.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+function background() {
+  return (
+    <>
+       <div className="absolute top-[5%] text-zinc-600  w-full py-10 flex justify-center text-xl font-semibold rounded-xl">Documents</div>
+       <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-zinc-900 text-[13vw] leading-none tracking-tighter '>Docs</h1>
+    </>
+  )
+}
+
+export default background
